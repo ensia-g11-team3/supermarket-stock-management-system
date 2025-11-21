ab98121ac9650be8680ab9daee0a9d6d77808294
anis <mohamed.anis.chehili@ensia.edu.dz>
2025-11-22 00:42:36 +0100

    login page creation

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
-import 'create_user.dart';
+import 'screens/login.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MainApp());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(useMaterial3: true),
-    home: const CreateUserScreen(),
-  );
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: LoginPage());
+  }
 }
