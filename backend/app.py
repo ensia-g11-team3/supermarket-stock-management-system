8fdb9a21d3b0a27e9ccefc05fcaf35373f808e34
Mustapha Boulefa <mustapha.boulefa@ensia.edu.dz>
2025-11-30 20:20:09 +0100

    pushing backend

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,19 @@
 # app.py
 from flask import Flask
-from routes.pos_transaction import pos_bp  # import blueprint
+from routes.pos_transaction import pos_bp
+from routes.users import users_bp
 from flask_cors import CORS
 
 app = Flask(__name__)
-
 CORS(app)
 
 @app.route("/")
 def home():
     return "Flask backend is running!"
 
-# register routes from save_transactions.py
+# Register blueprints
 app.register_blueprint(pos_bp)
+app.register_blueprint(users_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
