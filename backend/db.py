4121db05fa4c94a9badec6d86915f4609635efb4
Nour Malek Yahiaoui <nour.malek.yahiaoui@ensia.edu.dz>
2026-01-26 23:04:15 +0100

    Update db.py (remove Nour’s password)

--- backend/db.py
+++ backend/db.py
@@ -4,7 +4,7 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="MN25MY@110YAh",     # your MySQL password
+        password="",     # your MySQL password
         database="stock_db",
     )
 def close_connection(connection):
